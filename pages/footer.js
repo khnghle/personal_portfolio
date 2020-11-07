84b6f68929a092022926d0882b8b1716d0959895
Khuong <lekhuongy@gmail.com>
2020-11-07 10:26:44 -0600

    static home page

--- pages/footer.js
+++ pages/footer.js
@@ -1,5 +1,3 @@
-import { Fragment } from "react";
-
 export default function Footer() {
   return (
       <div>This is a test footer</div>
--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,22 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  function onClick (text) {
+    console.log('This should display something')
+  }
   return (
     <div className={styles.container}>
       <Head>
         <title>Coding WizKid</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Hello!</h1>
+
+      <h1>Hello, my name is Khuong and I'm 
+        <span> a software developer!</span>
+      </h1>
+
+      <button onClick={onClick}>Contact Me</button>
+      <button onClick={onClick}>Resume</button>
     </div>
   )
 }
