2ef373b507359ae24ae0bd888ab99e04fc1de498
Khuong <lekhuongy@gmail.com>
2021-01-05 15:40:20 -0600

    add meta data for image preview

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>Coding WizKid</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta property="og:image" content="profile.jpg" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossOrigin="anonymous"/>
         <link
           rel="stylesheet"
